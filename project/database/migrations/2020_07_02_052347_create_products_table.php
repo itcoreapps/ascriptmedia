f744f90f79c4f6cc83cf3a14193dcfa3772bf279
itcoreapps <67680960+itcoreapps@users.noreply.github.com>
2020-07-02 09:44:50 +0200

    Add files via upload

---
+++ project/database/migrations/2020_07_02_052347_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->integer('p_id')->autoIncrement();
+            $table->string('p_name');
+            $table->float('p_price_egp' , 8 ,2);
+            $table->float('p_price_dollar' , 8 ,2);
+            $table->float('p_price_bitcoins' , 8 , 8);
+            $table->string('p_image');
+            $table->string('p_video');
+            $table->integer('p_sales');
+            $table->string('p_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
